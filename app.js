34e15389df4c3e4d49bfc704833f712b019ba607
Pawarin Maion <pawarinmaion@MacBook-Air-khxng-Pawarin.local>
2022-11-15 21:15:23 +0700

    make it better2

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 8080;
 
-
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
 
 app.use(cors())
 app.use(bodyParser.json())
